a98ced9496fd9dc5204cb896a10980fe230a49fc
baijanathTharu <bnchaudhary.tharu@gmail.com>
2022-09-16 11:56:38 +0545

    :sparkles: handle errors in form with yup validation Schema

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,27 @@
-import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
 
-type FormData = {
-  firstName: string;
-  lastName: string;
-  age: number;
-};
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    age: yup.number().positive().integer().required(),
+  })
+  .required();
+
+type FormData = yup.InferType<typeof schema>;
 
 export default function FormComponent() {
   const {
     register,
-    setValue,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
   const onSubmit = handleSubmit((data) => console.log(data));
 
   return (
@@ -20,14 +29,29 @@ export default function FormComponent() {
       <div className='form-field'>
         <label>First Name</label>
         <input {...register('firstName')} />
+        {errors.firstName ? (
+          <div className='form-error-container'>
+            <p>{errors.firstName.message}</p>
+          </div>
+        ) : null}
       </div>
       <div className='form-field'>
         <label>Last Name</label>
         <input {...register('lastName')} />
+        {errors.lastName ? (
+          <div className='form-error-container'>
+            <p>{errors.lastName.message}</p>
+          </div>
+        ) : null}
       </div>
       <div className='form-field'>
         <label>Age</label>
         <input type='number' {...register('age')} />
+        {errors.age ? (
+          <div className='form-error-container'>
+            <p>{errors.age.message}</p>
+          </div>
+        ) : null}
       </div>
       <div className='form-field'>
         <button type='submit'>Submit</button>
